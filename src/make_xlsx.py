9df0dfb55d1332d4955bf7afad9117f543b3866c
Thibaut Cornolti <thibaut@cornolti.eu>
2018-04-05 12:11:08 +0200

    xlsx tab cost variations: limit to the 5 last months

--- src/make_xlsx.py
+++ src/make_xlsx.py
@@ -14,15 +14,15 @@ import utils
 SHEET_RESERVATIONS_SUMMARY = 1
 
 PRETTY_FIELD_NAMES = {
-    'instance_type'       : 'Instance type',
-    'availability_zone'   : 'Availability zone',
-    'tenancy'             : 'Tenancy',
-    'product'             : 'Product',
-    'count'               : 'Count',
-    'count_reserved'      : 'Count (reserved)',
-    'cost_ondemand'       : 'Cost (on demand)',
-    'cost_reserved_worst' : 'Cost (worst reserved)',
-    'cost_reserved_best'  : 'Cost (best reserved)',
+    'instance_type': 'Instance type',
+    'availability_zone': 'Availability zone',
+    'tenancy': 'Tenancy',
+    'product': 'Product',
+    'count': 'Count',
+    'count_reserved': 'Count (reserved)',
+    'cost_ondemand': 'Cost (on demand)',
+    'cost_reserved_worst': 'Cost (worst reserved)',
+    'cost_reserved_best': 'Cost (best reserved)',
 }
 
 PRETTY_FIELD_GROUPS = {
@@ -34,24 +34,26 @@ NUMFORMAT_CURRENCY = '#,##0.000 [$USD]'
 NUMFORMAT_PERCENT = '0.00%'
 NUMFORMAT_PERCENT_VAR = '\+0.00%;\-0.00%'
 
-IN_INSTANCE_RESERVATION_USAGE_DIR    = 'out/instance-reservation-usage'
-IN_RESERVATION_USAGE_DIR             = 'out/reservation-usage'
-IN_ABSOLUTE_COST_PER_MONTH           = 'out/absolute.csv'
+IN_INSTANCE_RESERVATION_USAGE_DIR = 'out/instance-reservation-usage'
+IN_RESERVATION_USAGE_DIR = 'out/reservation-usage'
+IN_ABSOLUTE_COST_PER_MONTH = 'out/absolute.csv'
 IN_INSTANCE_SIZE_RECOMMENDATIONS_DIR = 'out/instance-size-recommendation'
-IN_INSTANCE_HISTORY                  = 'out/instance-history.csv'
+IN_INSTANCE_HISTORY = 'out/instance-history.csv'
 
-COLOR_RED_BG   = "#ffcccc"
-COLOR_RED_FG   = "#cc0000"
+COLOR_RED_BG = "#ffcccc"
+COLOR_RED_FG = "#cc0000"
 COLOR_GREEN_BG = "#ccffcc"
 COLOR_GREEN_FG = "#006600"
+
 
 def _with_trailing(it, trail):
     return itertools.chain(it, itertools.repeat(trail))
 
+
 def reserved_summary(workbook, header_format, val_format):
     with utils.csv_folder(IN_INSTANCE_RESERVATION_USAGE_DIR) as records:
         worksheet = workbook.add_worksheet("Reserved instance summary")
-            
+
         worksheet.set_column("A:N", 15)
         worksheet.merge_range("A1:D1", "Reservation", header_format)
         worksheet.merge_range("E1:F1", "Count", header_format)
@@ -97,17 +99,21 @@ def reserved_summary(workbook, header_format, val_format):
             for h, v in line.items():
                 worksheet.write(i, refs[h][0], refs[h][2](v), refs[h][3])
             for h in ("cost_monthly_ondemand", "cost_monthly_reserved_worst", "cost_monthly_reserved_best"):
-                res = float(line["count"]) * float(line["cost_" + h[13:]]) * 720
+                res = float(line["count"]) * \
+                    float(line["cost_" + h[13:]]) * 720
                 worksheet.write_formula(
                     i, refs[h][0],
-                    "=F{}*{}{}*720".format(i+1, chr(ord('A') + refs[h][0] - 3), i+1), refs[h][3],
+                    "=F{}*{}{}*720".format(i+1, chr(ord('A') +
+                                                    refs[h][0] - 3), i+1), refs[h][3],
                     res,
                 )
             for h in ("savings_reserved_worst", "savings_reserved_best"):
-                res = 1 - float(line[h.replace("savings", "cost")]) / float(line["cost_ondemand"])
+                res = 1 - float(line[h.replace("savings", "cost")]
+                                ) / float(line["cost_ondemand"])
                 worksheet.write_formula(
                     i, refs[h][0],
-                    "=1-{}{}/G{}".format(chr(ord('A') + refs[h][0] - 5), i+1, i+1), refs[h][3],
+                    "=1-{}{}/G{}".format(chr(ord('A') +
+                                             refs[h][0] - 5), i+1, i+1), refs[h][3],
                     res,
                 )
             worksheet.conditional_format("E{}".format(i+1), {
@@ -121,7 +127,7 @@ def reserved_summary(workbook, header_format, val_format):
 def reservation_usage_summary(workbook, header_format, val_format):
     with utils.csv_folder(IN_RESERVATION_USAGE_DIR) as records:
         worksheet = workbook.add_worksheet("Reservation usage summary")
-            
+
         worksheet.set_column("A:J", 18)
         worksheet.merge_range("A1:D1", "Reservation", header_format)
         worksheet.merge_range("E1:F1", "Count", header_format)
@@ -151,7 +157,8 @@ def reservation_usage_summary(workbook, header_format, val_format):
         for i, line in zip(itertools.count(2), records):
             for h, v in line.items():
                 worksheet.write(i, refs[h][0], refs[h][2](v), refs[h][3])
-            effective_cost = float(line["cost_upfront"]) / 720 + float(line["cost_hourly"])
+            effective_cost = float(
+                line["cost_upfront"]) / 720 + float(line["cost_hourly"])
             worksheet.write_formula(
                 i, refs["effective_cost"][0],
                 "=G{}/720+H{}".format(*[i+1]*2), refs["effective_cost"][3],
@@ -159,23 +166,26 @@ def reservation_usage_summary(workbook, header_format, val_format):
             )
             worksheet.write(
                 i, refs["monthly_losses"][0],
-                "=(E{}-F{})*I{}*720".format(*[i+1]*3), refs["monthly_losses"][3],
-                (float(line["count"]) - float(line["count_used"])) * effective_cost * 720,
+                "=(E{}-F{})*I{}*720".format(*[i+1]
+                                            * 3), refs["monthly_losses"][3],
+                (float(line["count"]) - float(line["count_used"])
+                 ) * effective_cost * 720,
             )
 
 
 def weekly_variations(workbook, header_format, val_format):
-    to_alpha = lambda x: chr(ord('A') + x)
+    def to_alpha(x): return chr(ord('A') + x)
 
     with open(IN_ABSOLUTE_COST_PER_MONTH) as f:
         reader = csv.DictReader(f)
         source = sorted(
             reader,
-            key=(lambda row: sum(float(v) for k, v in row.items() if k != 'usage')),
+            key=(lambda row: sum(float(v)
+                                 for k, v in row.items() if k != 'usage')),
             reverse=True,
         )
         worksheet = workbook.add_worksheet("Cost variations")
-            
+
         worksheet.set_column("A:A", 30)
         worksheet.set_column("B:M", 14)
         worksheet.merge_range("A1:A3", "Usage type", header_format)
@@ -202,11 +212,15 @@ def weekly_variations(workbook, header_format, val_format):
         per_format.set_border()
         per_format.set_num_format(NUMFORMAT_PERCENT)
 
+        date_fieldnames = reader.fieldnames[1:-1]
+        if len(date_fieldnames) > 6:
+            date_fieldnames = date_fieldnames[-5:]
+        print(date_fieldnames)
         refs = {
             header: [i, True, float]
-            for i, header in zip(itertools.count(3, 2), reader.fieldnames[2:])
+            for i, header in zip(itertools.count(3, 2), date_fieldnames[1:])
         }
-        refs[reader.fieldnames[1]] = [1, False, float]
+        refs[date_fieldnames[0]] = [1, False, float]
         refs["usage"] = [0, False, str]
         for h, v in refs.items():
             if v[1]:
@@ -217,43 +231,55 @@ def weekly_variations(workbook, header_format, val_format):
                 worksheet.write(1, v[0], h, header_format)
                 worksheet.write(2, v[0], "Cost", header_format)
         for i, line in zip(itertools.count(3), source):
-            for h, v in line.items():
-                worksheet.write(i, refs[h][0], refs[h][2](v), cur_format)
-                if refs[h][1]:
-                    before = float(line[reader.fieldnames[int(refs[h][0]/2)]])
+            for name, meta in refs.items():
+                val = line[name]
+                worksheet.write(i, meta[0], meta[2](val), cur_format)
+                if meta[1]:
+                    before = float(line[date_fieldnames[int(meta[0]/2-1)]])
                     worksheet.write_formula(
-                        i, refs[h][0]-1,
-                        "=IF({}{}=0,\"\",D{}/B{}-1)".format(*[to_alpha(refs[h][0] - 2)]+[i+1]*3), per_format,
-                        " " if before == 0.0 else refs[h][2](v) / before - 1
+                        i, meta[0]-1,
+                        "=IF({}{}=0,\"\",{}{}/{}{}-1)".format(
+                            to_alpha(meta[0] - 2),
+                            i+1,
+                            to_alpha(meta[0]),
+                            i+1,
+                            to_alpha(meta[0] - 2),
+                            i+1,
+                        ), per_format,
+                        " " if before == 0.0 else meta[2](val) / before - 1
                     )
-                    worksheet.conditional_format("{}{}".format(to_alpha(refs[h][0]-1), i+1), {
+                    worksheet.conditional_format("{}{}".format(to_alpha(meta[0]-1), i+1), {
                         "type": "cell",
                         "criteria": "greater than",
                         "value": "0",
                         "format": red_format,
                     })
-                    worksheet.conditional_format("{}{}".format(to_alpha(refs[h][0]-1), i+1), {
+                    worksheet.conditional_format("{}{}".format(to_alpha(meta[0]-1), i+1), {
                         "type": "cell",
                         "criteria": "less than or equal to",
                         "value": "0",
                         "format": green_format,
                     })
-            worksheet.write("M{}".format(i+1), sum([float(line[o]) for o in reader.fieldnames[1:]]), cur_format)
+            worksheet.write("M{}".format(
+                i+1), sum([float(line[o]) for o in reader.fieldnames[1:]]), cur_format)
 
 
 def instance_history(workbook, header_format, val_format):
     with open(IN_INSTANCE_HISTORY) as f:
         reader = csv.DictReader(f)
         worksheet = workbook.add_worksheet("Instance count history")
-        
+
         worksheet.set_column(0, len(reader.fieldnames)-1, 18)
         worksheet.merge_range("A1:A2", "Date", header_format)
-        worksheet.merge_range(0, 1, 0, len(reader.fieldnames)-1, "Instance Count", header_format)
+        worksheet.merge_range(0, 1, 0, len(
+            reader.fieldnames)-1, "Instance Count", header_format)
 
         def transform(x):
             try:
-                if x == "": return 0
-                else: return int(x)
+                if x == "":
+                    return 0
+                else:
+                    return int(x)
             except ValueError:
                 return x
 
